5f51ad2379a2ec2a9db699dffc992c99b2b3e522
Chathura Hewa Algiriyage <cpriyankara@ecs.vuw.ac.nz>
2021-11-17 03:37:22 +1300

    Fixed a post method issue.

--- devices-server.js
+++ devices-server.js
@@ -25,7 +25,7 @@ fs.createReadStream('timezone.csv')
     .on('data', (data) =>
         fileContents.push(data)
     ).on('end', async () => {
-    await formatDate(fileContents);
+    await formatDates(fileContents);
 });
 
 app.get('/get-devices', function (req, res) {
@@ -35,7 +35,7 @@ app.get('/get-devices', function (req, res) {
 app.post('/device', urlencodedParser, function (req, res) {
     let deviceFound = false;
     fileContents.forEach((device) => {
-        if (device['id'] === req.body.id) {
+        if (device.id === req.body.id) {
             deviceFound = true;
             res.send(JSON.stringify(device));
         }
@@ -88,12 +88,12 @@ watch('timezone.csv', function (event, filename) {
             for (let i = 0; i < fileContents.length; i++) {
                 //On device location change
                 if (fileContents[i].lat !== modifiedFileContents[i].lat || fileContents[i].lng !== modifiedFileContents[i].lng) {
-                    fileContents[i] = modifiedFileContents[i];
+                    fileContents[i] = await formatSingleDate(modifiedFileContents[i]);
                     modifiedDevices.push(modifiedFileContents[i])
                 }
             }
-            await formatDate(fileContents);
-            await formatDate(modifiedDevices);
+            //await formatDates(fileContents);
+            await formatDates(modifiedDevices);
             modifiedDevices.forEach((device) => {
                 clients.forEach((client) => {
                     if (device['id'] === client.req) {
@@ -104,43 +104,47 @@ watch('timezone.csv', function (event, filename) {
         });
 });
 
-async function formatDate(fileData) {
+async function formatDates(fileData) {
     for (const data of fileData) {
-        let timestamp = data.timestamp_utc;
-        let longitude = data.lng;
-        let latitude = data.lat;
-        let timezone;
-        let rdata;
-        let timezone1
-        const config = {
-            method: 'get',
-            url: `https://maps.googleapis.com/maps/api/timezone/json?location=${latitude}%2C${longitude}&timestamp=${timestamp}&key=${key}`,
-            headers: {}
-        };
-        await axios(config)
-            .then(function (response) {
-                rdata = response.data;
-                timezone1 = rdata.timeZoneId;
-
-                const date = new Date(timestamp * 1000);
-                timezone = geoTz(latitude, longitude);
-                const nDate = date.toLocaleString('en-GB', {
-                    timeZone: timezone
-                });
-                let time = date.toLocaleTimeString('en-GB', {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    timeZone: timezone,
-                    hour12: false
-                });
-                let date1 = date.toLocaleDateString()
-                let time1 = date.toTimeString('en-US', {timeZone: timezone});
-                data['date_time'] = `${date1} ${time1} ${timezone} ${getAbbreviation(rdata.timeZoneName)} ${getoffset(rdata)}`;
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        await formatSingleDate(data);
     }
+}
+
+async function formatSingleDate(data) {
+    let timestamp = data.timestamp_utc;
+    let longitude = data.lng;
+    let latitude = data.lat;
+    let timezone;
+    let rdata;
+    let timezone1
+    const config = {
+        method: 'get',
+        url: `https://maps.googleapis.com/maps/api/timezone/json?location=${latitude}%2C${longitude}&timestamp=${timestamp}&key=${key}`,
+        headers: {}
+    };
+    await axios(config)
+        .then(function (response) {
+            rdata = response.data;
+            timezone1 = rdata.timeZoneId;
+
+            const date = new Date(timestamp * 1000);
+            timezone = geoTz(latitude, longitude);
+            const nDate = date.toLocaleString('en-GB', {
+                timeZone: timezone
+            });
+            let time = date.toLocaleTimeString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: timezone,
+                hour12: false
+            });
+            let date1 = date.toLocaleDateString()
+            let time1 = date.toTimeString('en-US', {timeZone: timezone});
+            data['date_time'] = `${date1} ${time1} ${timezone} ${getAbbreviation(rdata.timeZoneName)} ${getoffset(rdata)}`;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function getAbbreviation(text) {
